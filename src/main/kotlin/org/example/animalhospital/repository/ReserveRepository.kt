eaa421c55a2c86886342d3a2d2be30c13b3e6635
parkseongryel <tjdfuf3413@gmail.com>
2024-01-31 09:05:45 +0900

    repo

--- src/main/kotlin/org/example/animalhospital/repository/ReserveRepository.kt
+++ src/main/kotlin/org/example/animalhospital/repository/ReserveRepository.kt
@@ -2,14 +2,14 @@ package org.example.animalhospital.repository
 
 import org.example.animalhospital.entity.Reserve
 import org.example.animalhospital.entity.dto.ReserveRequest
+import org.example.animalhospital.entity.enums.PaymentType
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface ReserveRepository: JpaRepository<Long, Long> {
-    fun findReserveById(keys: Set<Long>): MutableList<Reserve>
+interface ReserveRepository: JpaRepository<Reserve, Long> {
+    fun findReserveByUserId(keys: Set<Long>): MutableList<Reserve>
     fun getAll(pageable: Pageable): List<ReserveRequest>
     fun findReservationByUserId(pageable: Pageable, userId: Long): MutableList<ReserveRequest>
-    fun payment(request: ReserveRequest, userId: Long): Boolean
 }
