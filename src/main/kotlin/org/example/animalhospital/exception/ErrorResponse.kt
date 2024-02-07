459a3dcc03a2b8b0ee53289e73990c771defe04c
parkseongryel <tjdfuf3413@gmail.com>
2024-02-07 10:16:01 +0900

    exception

--- src/main/kotlin/org/example/animalhospital/exception/ErrorResponse.kt
+++ src/main/kotlin/org/example/animalhospital/exception/ErrorResponse.kt
@@ -1,10 +1,5 @@
 package org.example.animalhospital.exception
 
-import lombok.Getter
-import lombok.Setter
-
-@Getter
-@Setter
 data class ErrorResponse(
     var code: ResultCode? = null,
     var message: String? = null
--- src/main/kotlin/org/example/animalhospital/exception/Response.kt
+++ src/main/kotlin/org/example/animalhospital/exception/Response.kt
@@ -1,10 +1,5 @@
 package org.example.animalhospital.exception
 
-import lombok.Getter
-import lombok.Setter
-
-@Getter
-@Setter
 data class Response<T>(
     val resultCode: String = ResultCode.SUCCESS.name,
     val data: T? = null,
