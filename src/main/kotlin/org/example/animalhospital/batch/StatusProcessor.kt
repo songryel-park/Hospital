81826e70e590b835b3333a5121fc95c3f4b8a01b
parkseongryel <tjdfuf3413@gmail.com>
2024-02-15 15:14:19 +0900

    동시성 이슈 Locking

--- src/main/kotlin/org/example/animalhospital/batch/StatusProcessor.kt
+++ src/main/kotlin/org/example/animalhospital/batch/StatusProcessor.kt
@@ -2,22 +2,20 @@ package org.example.animalhospital.batch
 
 import org.example.animalhospital.entity.Reserve
 import org.example.animalhospital.entity.enums.ReserveStatus
-import org.example.animalhospital.exception.NotFoundException
 import org.example.animalhospital.repository.ReserveRepository
 import org.springframework.batch.item.ItemProcessor
 import org.springframework.stereotype.Component
-import java.time.LocalDateTime
+import org.springframework.transaction.annotation.Isolation
+import org.springframework.transaction.annotation.Transactional
 
 @Component
 class StatusProcessor(private val repository: ReserveRepository): ItemProcessor<Reserve?, Reserve?> {
+    //@Transactional(isolation = Isolation.REPEATABLE_READ)
     override fun process(item: Reserve): Reserve? {
-        val reservation = repository.findById(item.reserveId!!)
-            .orElseThrow { NotFoundException("해당 예약을 찾을 수 없습니다.") }
-
-        if (reservation.status == ReserveStatus.RESERVATION && reservation.reserveDate == LocalDateTime.now().minusDays(1L)) {
-            reservation.status = ReserveStatus.RESERVED
-            return repository.save(reservation)
+        val locked = repository.findByIdWithLock(item.reserveId!!)
+        if (locked!!.status == ReserveStatus.RESERVATION) {
+            locked.status = ReserveStatus.RESERVED
         }
-        return null
+        return locked
     }
 }
--- src/main/kotlin/org/example/animalhospital/repository/ReserveRepository.kt
+++ src/main/kotlin/org/example/animalhospital/repository/ReserveRepository.kt
@@ -1,17 +1,24 @@
 package org.example.animalhospital.repository
 
+import jakarta.persistence.LockModeType
 import org.example.animalhospital.entity.Reserve
 import org.example.animalhospital.entity.enums.ReserveStatus
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Lock
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 import java.time.LocalDateTime
 
 @Repository
 interface ReserveRepository: JpaRepository<Reserve, Long> {
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT r FROM Reserve r WHERE r.reserveId = :reserveId")
+    fun findByIdWithLock(@Param("reserveId") reserveId: Long): Reserve?
     fun findByReserveDateBefore(reserveDate: LocalDateTime): List<Reserve>
-    fun findByReserveDateBeforeAndStatus(reserveDate: LocalDateTime, status: ReserveStatus): List<Reserve>
+    fun findByReserveDateBetween(start: LocalDateTime, end: LocalDateTime): List<Reserve>
     override fun findAll(pageable: Pageable): Page<Reserve>
     fun findReservationByUserId(pageable: Pageable, userId: Long): MutableList<Reserve>
 }
